0e2c0e868b3abf306aa4e29d47ec536f79db739a
vivek singh <55220203+ui-vivek@users.noreply.github.com>
2023-03-10 18:11:49 +0530

    Update BuyUme.js

--- BuyUme.js
+++ BuyUme.js
@@ -1,21 +1,38 @@
-// Define the array and the number of rotations
-const arr = [1,2,3,4,5,6,7,8,9,10];
-const k = 3;
+function rotateArray(arr, k) {
+  // First, normalize k in range [0, arr.length)
+  k = k % arr.length;
 
-// Perform K rotations
-for (let i = 0; i < k; i++) {
-  // Remove the last element of the array and save it
-  const lastElement = arr[arr.length - 1];
-  // Move all other elements one position to the right
-  for (let j = arr.length - 1; j > 0; j--) {
-    arr[j] = arr[j - 1];
+  // Reverse the 1st part of the array, up to k elements
+  for (let i = 0, j = k - 1; i < j; i++, j--) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-  // Put the last element at the beginning of the array
-  arr[0] = lastElement;
+
+  // Reverse the 2nd part of the arr, from k to the end
+  for (let i = k, j = arr.length - 1; i < j; i++, j--) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // Reverse the entire array
+  for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // Return the rotated array
+  return arr;
 }
 
-// Print the rotated array
-console.log(arr);
+// Example usage:
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const k = 3;
+const rotatedArr = rotateArray(arr, k);
+console.log(rotatedArr); // [4, 5, 6, 7, 8, 9, 10, 1, 2, 3]
+
 
 // Another way
 // -------------------------------------------------------------

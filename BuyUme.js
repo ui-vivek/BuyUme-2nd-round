6ecb303ffe862f69bc41c3fc79b210f55d3f530d
vivek singh <55220203+ui-vivek@users.noreply.github.com>
2023-03-10 18:12:29 +0530

    Update BuyUme.js

--- BuyUme.js
+++ BuyUme.js
@@ -34,35 +34,4 @@ const rotatedArr = rotateArray(arr, k);
 console.log(rotatedArr); // [4, 5, 6, 7, 8, 9, 10, 1, 2, 3]
 
 
-// Another way
-// -------------------------------------------------------------
 
-// function rotateArray(arr, k) {
-//     const n = arr.length;
-//     k = k % n; // ensure k is within range [0, n)
-  
-//     // reverse the first n-k elements
-//     for (let i = 0, j = n - k - 1; i < j; i++, j--) {
-//       [arr[i], arr[j]] = [arr[j], arr[i]]; // swap elements
-//     }
-  
-//     // reverse the last k elements
-//     for (let i = n - k, j = n - 1; i < j; i++, j--) {
-//       [arr[i], arr[j]] = [arr[j], arr[i]]; // swap elements
-//     }
-  
-//     // reverse the entire array
-//     for (let i = 0, j = n - 1; i < j; i++, j--) {
-//       [arr[i], arr[j]] = [arr[j], arr[i]]; // swap elements
-//     }
-  
-//     return arr;
-//   }
-  
-//   // example usage:
-//   const arr = [1,2,3,4,5,6,7,8,9,10];
-
-//   const k = 3;
-//   const rotated = rotateArray(arr, k);
-//   console.log(rotated); // [4,5,6,7,8,9,10,1,2,3]
-  
